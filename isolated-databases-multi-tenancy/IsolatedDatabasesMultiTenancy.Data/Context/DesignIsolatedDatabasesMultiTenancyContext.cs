81a668737bace6a10aa7a89f7aa0ba39b18b5102
Gleryston Matos <glerystonmatos@yahoo.com.br>
2023-05-14 11:41:11 -0300

    IsolatedDatabasesMultiTenancy: Instalação do Microsoft.EntityFrameworkCore.Tools no projeto Api Ajuste nas strinsgs de conexão para ambientes de teste

--- isolated-databases-multi-tenancy/IsolatedDatabasesMultiTenancy.Data/Context/DesignIsolatedDatabasesMultiTenancyContext.cs
+++ isolated-databases-multi-tenancy/IsolatedDatabasesMultiTenancy.Data/Context/DesignIsolatedDatabasesMultiTenancyContext.cs
@@ -9,7 +9,7 @@ namespace IsolatedDatabasesMultiTenancy.Data.Context
         public IsolatedDatabasesMultiTenancyContext CreateDbContext(string[] args)
         {
             DbContextOptionsBuilder<IsolatedDatabasesMultiTenancyContext> builder = new DbContextOptionsBuilder<IsolatedDatabasesMultiTenancyContext>();
-            builder.UseSqlServer("Data Source=10.0.0.131\\SQLEXPRESS;Initial Catalog=IsolatedDatabasesMultiTenancy00;Persist Security Info=True;User ID=sa;Password=1234");
+            builder.UseSqlServer("Data Source=10.0.0.131\\SQLEXPRESS;Initial Catalog=IsolatedDatabasesMultiTenancy00;Persist Security Info=True;User ID=sa;Password=1234;Encrypt=False");
             return new IsolatedDatabasesMultiTenancyContext(builder.Options, new TenantService(), null);
         }
     }
